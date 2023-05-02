ff839697a04fce9a86f93e53629e7d8507b0720f
franciscojrdev <franciscojunior7222@gmail.com>
2023-05-02 20:21:24 -0300

    fix/sync: connect database

--- src/controllers/maps-controller.js
+++ src/controllers/maps-controller.js
@@ -1,38 +1,45 @@
 const Location = require("../schemas/map-schema");
 
-exports.getLocation = (req, res, next) => {
-  Location.findAll()
-    .then((locations) => {
-      res.status(200).json({ locations: locations });
-    })
-    .catch((err) => console.log(err));
+const getLocations = async (req, res) => {
+  try {
+    const eventsData = await Location.findAll();
+    res.status(200).send(eventsData);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
-exports.getLocationName = (req, res, next) => {
-  const locationName = req.params.locationName;
-  Location.findByName(locationName)
-    .then((location) => {
-      if (!location) {
-        return res.status(404).json({ message: "Localização não encontrada!" });
-      }
-      res.status(200).json({ location: location });
-    })
-    .catch((err) => console.log(err));
+// exports.getLocationName = (req, res, next) => {
+//   const locationName = req.params.locationName;
+//   Location.findByName(locationName)
+//     .then((location) => {
+//       if (!location) {
+//         return res.status(404).json({ message: "Localização não encontrada!" });
+//       }
+//       res.status(200).json({ location: location });
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+const createLocation = async (req, res) => {
+  const { name, description,longitude,latitude} = req.body;
+  try {
+  
+    const createdEvent = Location.create({
+      name: name,
+      description: description,
+      latitude,
+      longitude
+    });
+    if (createdEvent) res.status(201).send("Event created");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-exports.createLocation = (req, res, next) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  Location.create({
-    name: name,
-    description: description,
-  })
-    .then((result) => {
-      console.log("Localização criada");
-      res.status(201).json({
-        message: "Localização criada com sucesso",
-        location: result,
-      });
-    })
-    .catch((err) => console.log(err));
+const sincronize = async (req, res) => {
+  await Location.sync();
+  res.status(200).send("Sincronizado");
 };
+
+module.exports = { getLocations, sincronize, createLocation };
--- src/schemas/map-schema.js
+++ src/schemas/map-schema.js
@@ -1,15 +1,32 @@
 const Sequelize = require("sequelize");
-const db = require("../config/database");
+const sequelize = require("../config/database");
 
-const Location = db.define("location", {
+const Location = sequelize.define("location", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
-    allowNull: false,
     primaryKey: true,
   },
-  name: Sequelize.STRING,
-  description: Sequelize.STRING,
+  name: {
+    type: Sequelize.STRING
+  },
+  description: { 
+    type: Sequelize.STRING 
+  },
+  latitude:{
+    type: Sequelize.FLOAT
+  },
+  longitude:{
+    type: Sequelize.FLOAT
+  }
+  // geometria:{
+  //   type: Sequelize.GEOMETRY
+  // },
 });
+
+(async () => {
+  await sequelize.sync({ force: true });
+  // Code here
+})();
 
 module.exports = Location;
